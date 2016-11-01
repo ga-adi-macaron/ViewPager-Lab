ae6e08f1e9b71928f614ba68ee8b768c51784fd0
cbrads61 <cbrads61@gmail.com>
2016-11-01 12:58:06 -0400

    Colin Bradley ViewPagerLab

---
+++ ViewPagerLab/app/src/main/java/com/colinbradley/viewpagerlab/MainActivity.java
@@ -0,0 +1,26 @@
+package com.colinbradley.viewpagerlab;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ViewPager viewPager = (ViewPager)findViewById(R.id.view_pager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
