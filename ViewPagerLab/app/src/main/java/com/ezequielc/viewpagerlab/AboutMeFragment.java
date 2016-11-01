de883a78d71efa182549be8c0ae43f1156ecf0b2
ECarrasco91 <ezequielcarrasco91@yahoo.com>
2016-11-01 13:37:44 -0400

    file added

---
+++ ViewPagerLab/app/src/main/java/com/ezequielc/viewpagerlab/AboutMeFragment.java
@@ -0,0 +1,21 @@
+package com.ezequielc.viewpagerlab;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by student on 11/1/16.
+ */
+
+public class AboutMeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_aboutme, container, false);
+    }
+}
